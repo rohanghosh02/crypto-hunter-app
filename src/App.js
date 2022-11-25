dec990d2ef7af1155e64ea36198116528c0455b7
Rohan <rohankingkumar8@gmail.com>
2022-11-25 19:17:26 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import '../src/styles/App.css';
+import React ,{useReducer}from 'react'
+import "../src/styles/App.css";
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT:'delete-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  EVALUATE:'evaluate'
 
+}
+
+function reducer(state,{type,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand:`${currentOperand}`
+      }
+  }
+
+}
 function App() {
+
+
+  const [{previousOperand,currentOperand,operation}, dispatch] = useReducer(reducer,{});
   return (
-    <div className="App">
-      <header className="App-header">
-        Hi
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand}{operation}</div>
+        <div className="current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
